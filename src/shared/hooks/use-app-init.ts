9a5c8a1cc6ae18b144c797ace12e39ef21e48551
almonsour13 <almonsoursalida@gmail.com>
2026-05-02 23:23:17 +0800

    chore: created hooks to initialzied settings and profile

---
+++ src/shared/hooks/use-app-init.ts
@@ -0,0 +1,34 @@
+import { useProfileStore } from "@/shared/stores/use-profile.store";
+import { useEffect, useState } from "react";
+import { profileService } from "../services/storage/profile.storage.services";
+import { settingsService } from "../services/storage/settings.storage.service";
+import { useSettingsStore } from "../stores/use-settings.store";
+
+export const useAppInit = () => {
+    const [ready, setReady] = useState(false);
+
+    const setProfile = useProfileStore((s) => s.setProfile);
+    const setSettings = useSettingsStore((s) => s.setSettings);
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const [profile, settings] = await Promise.all([
+                    profileService.getProfile(),
+                    settingsService.getSettings(),
+                ]);
+
+                setProfile(profile);
+                setSettings(settings);
+            } catch (e) {
+                console.error("[useAppInit] Failed to load app data:", e);
+            } finally {
+                setReady(true);
+            }
+        };
+
+        init();
+    }, []);
+
+    return { ready };
+};
